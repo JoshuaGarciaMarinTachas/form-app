fb55c1718b1c85f7c0af7647186c9007a39abbb8
JoshuaGarciaMarinTachas <141855424+JoshuaGarciaMarinTachas@users.noreply.github.com>
2026-05-11 12:35:57 -0600

    correccion

--- js/auth.js
+++ js/auth.js
@@ -1,18 +1,10 @@
 import {
   getAuth,
   signInWithEmailAndPassword,
-  signOut,
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const auth = getAuth();
 
-// LOGIN REAL
 export async function login(email, pass) {
-  const userCredential = await signInWithEmailAndPassword(auth, email, pass);
-  return userCredential.user;
-}
-
-// LOGOUT
-export async function logout() {
-  await signOut(auth);
+  return await signInWithEmailAndPassword(auth, email, pass);
 }
